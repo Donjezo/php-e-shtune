5eb33482c43b3a2c6b9d8f0ec6f69f2bfd9626a1
Donjetë Zogaj <70452463+Donjezo@users.noreply.github.com>
2025-10-18 14:19:05 +0200

    day 3

---
+++ day3/forearch.php
@@ -0,0 +1,14 @@
+
+
+<?php
+   $users=array("Eldoni","Ylli","Trimi","Erona","Lora","Liza","Rudina","Dua");
+
+
+
+
+    foreach($users as $perdorusi){
+         echo"$perdorusi <br> ";
+    }
+
+
+?>
